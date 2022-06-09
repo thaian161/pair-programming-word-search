10761273f80de3cc4d2729b8a52cfd9ad0bd39b0
thaian161 <thaian161@yahoo.com>
2022-06-08 20:29:38 -0500

    pair programming with Shrey Patel

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,53 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  if (word === undefined || word > letters.length) return false;
 
-module.exports = wordSearch
+  const horizontalJoin = letters.map((ls) => ls.join(''));
+  console.log(horizontalJoin); //=> return array of string of each row
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+    if (l.reverse.includes(word)) return true;
+  }
+
+  let columnLength = letters[0].length; //8
+  const columnArray = [];
+
+  for (let i = 0; i < columnLength; i++) {
+    columnArray.push([]); //we push [] into our new columnArray => 8 new emty array based on the columnLength 
+  }
+
+  for (let i = 0; i < columnLength; i++) {
+    for (let letterRow of letters) {
+      columnArray[i].push(letterRow[i]);
+    }
+  }
+
+  const verticalJoin = columnArray.map((ls) => ls.join(''));
+
+  console.log('verticalJoinAray:', verticalJoin);
+  for (l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  return false;
+};
+
+// //====TEST CODE=====
+// const matrix = [
+//   ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//   ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+//   ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//   ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+//   ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+//   ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+//   ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+//   ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+//   ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+// ];
+// const keyword = 'FRANK'; //false
+
+// wordSearch(matrix, keyword);
+
+module.exports = wordSearch;
+
+//BACKWARD CHECK FROM THE END
+//DIAGO : reverse
